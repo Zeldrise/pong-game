7c4d3ec1607e08e8ee6c7d554d8b1e9db13d5ef6
Zeldrise <fandiekapratama.0612@gmail.com>
2023-09-26 14:51:54 +0700

    first commit

---
+++ pong-0/main.lua
@@ -0,0 +1,19 @@
+WINDOWS_WIDTH = 1280
+WINDOWS_HEIGHT = 720
+
+function love.load()
+love.window.setMode(WINDOWS_WIDTH, WINDOWS_HEIGHT, {
+  fullscreen = false,
+  resizable = false,
+  vsync = true
+})
+end
+
+function love.draw()
+  love.graphics.printf(
+    'Fandi Eka!',
+    0,
+    WINDOWS_HEIGHT - 720,
+    WINDOWS_WIDTH ,
+  'right')
+  end
